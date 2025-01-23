795f8e33c9dd7016ab862cf51dcfe511672db899
rdncruz <118281239+rdncruz@users.noreply.github.com>
2025-01-23 14:18:12 +0800

    creating-user

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @auth
+        <h1>Hello</h1>
+    @else
+    <div style="border:  8px solid black; padding:15px">
+        <h1>Register</h1>
+        <form action="/register" method="POST">
+            @csrf
+            <input type="text" name="name" placeholder="username">
+            <input type="text" name="email" placeholder="gmail">
+            <input type="password" name="password" placeholder="password">
+            <button>register</button>
+        </form>
+    </div>
+
+    @endauth
+    
+   
+    
+
+
+
+</body>
+</html>
